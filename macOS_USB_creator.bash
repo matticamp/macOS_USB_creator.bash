3c6aabb2a16e779681c302ae7f4e8e3a9ba3e435
matticamp <2312425+matticamp@users.noreply.github.com>
2018-03-03 11:32:38 -0500

    Add files via upload

---
+++ macOS_USB_creator.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume --nointeraction --applicationpath /Applications/Install\ macOS\ High\ Sierra.app
