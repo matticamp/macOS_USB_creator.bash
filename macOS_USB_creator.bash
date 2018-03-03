555d9e58f93fa521727ef9ef64a163cd4aa16c60
matticamp <2312425+matticamp@users.noreply.github.com>
2018-03-03 11:43:12 -0500

    Update macOS_USB_creator.bash

--- macOS_USB_creator.bash
+++ macOS_USB_creator.bash
@@ -2,7 +2,8 @@
 #Created by Mattia Campagnano on Sat Mar 3 11:33:22 EST 2018
 
 #Bash script leveraging a native macOS command in order to create a bootable USB installer. 
-#The lengthy command forming the bulk of this script copies all the files included in the macOS installer, that must be previously downloaded from the App Store, to a USB drive and makes it bootable.
+#The lengthy command forming the bulk of this script copies all the files included in the macOS 
+#installer, that must be previously downloaded from the App Store, to a USB drive and makes it bootable.
 
 #macOS installer will be automatically downloaded to the Applications directory, once the download is completed.
 
